0f22a53f894c82b5442cde055a471cab5cf670e2
tpstocker <74962602+tpstocker@users.noreply.github.com>
2021-02-23 15:08:21 +0000

    Add files via upload

---
+++ lab_full_stack_post_birds_start/client/src/App.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+import SightingsForm from "./components/SightingsForm";
+import SightingsGrid from "./containers/SightingsGrid";
+import { getSightings } from "./services/SightingService";
+
+function App() {
+  const [birdSightings, setBirdSightings] = useState([]);
+
+  useEffect(() => {
+    getSightings().then((allSightings) => {
+      setBirdSightings(allSightings);
+    });
+  }, []);
+
+  const addSighting = (sighting) => {
+    const temp = birdSightings.map((s) => s);
+    temp.push(sighting);
+    setBirdSightings(temp);
+  };
+
+  const removeSighting = (id) => {
+    const temp = birdSightings.map((s) => s);
+    const indexToDel = temp.map((s) => s._id).indexOf(id);
+    console.log(indexToDel);
+
+    temp.splice(indexToDel, 1);
+    setBirdSightings(temp);
+  };
+
+  return (
+    <>
+      <SightingsForm addSighting={addSighting} />
+      <SightingsGrid
+        sightings={birdSightings}
+        removeSighting={removeSighting}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ lab_full_stack_post_birds_start/client/src/containers/SightingsGrid.js
@@ -0,0 +1,10 @@
+import SightingCard from "../components/SightingCard";
+const SightingsGrid = ({ sightings, removeSighting }) => {
+  const sightingsList = sightings.map((sighting) => {
+    return <SightingCard sighting={sighting} removeSighting={removeSighting} />;
+  });
+
+  return <>{sightingsList}</>;
+};
+
+export default SightingsGrid;
